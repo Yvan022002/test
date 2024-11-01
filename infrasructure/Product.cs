4282237b6fc416c344a213b48e3a13c8253cfca0
Your Name <nguy1601@usherbrooke.ca>
2024-11-01 09:24:09 -0400

    add product.cs and user.cs

---
+++ infrasructure/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication1.infrasructure
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+      
+
+    public Product(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            
+        }
+
+    }
+}
---
+++ infrasructure/User.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication1.infrasructure
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User(string Id,string Name,string Email,string Password) { 
+            this.Id = Id;
+            this.Name = Name;
+            this.Email = Email;
+            this.Password = Password;
+        }
+
+    }
+}
