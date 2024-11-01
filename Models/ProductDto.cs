f1af73b09e5db4719c2d651bbdedc20d6e6b07fe
Your Name <nguy1601@usherbrooke.ca>
2024-11-01 09:21:26 -0400

    add model product

---
+++ Models/ProductDto.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication1.Models
+{
+    public class ProductDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+
+        public ProductDto(string name, string description,decimal price)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+
+        }
+    }
+}
