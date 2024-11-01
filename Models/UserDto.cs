a59abf88f0613c3c15981fcda266fbb770b42427
Your Name <nguy1601@usherbrooke.ca>
2024-11-01 09:23:03 -0400

    add model User

---
+++ Models/UserDto.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1.Models
+{
+    public class UserDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public UserDto(string Name, string Email, string Password)
+        {
+            this.Name = Name;
+            this.Email = Email;
+            this.Password = Password;
+        }
+    }
+}
